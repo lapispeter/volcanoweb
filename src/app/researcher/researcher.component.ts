d183745f3fa3f909a7342057e33fbdd06d533942
Lapis Péter <lapispeter69@gmail.com>
2026-02-14 10:04:46 +0100

    componensek service legenerálva

---
+++ src/app/researcher/researcher.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-researcher',
+  imports: [],
+  templateUrl: './researcher.component.html',
+  styleUrl: './researcher.component.css',
+})
+export class ResearcherComponent {
+
+}
